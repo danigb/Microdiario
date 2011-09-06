e15eaed877e479fe3e40b9e67422cae43fc70b7d
danigb <danigb@gmail.com>
2011-09-06 21:59:36 +0200

    Rails 3.1 mongoid rspec cucumber setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Microdiario::Application.config.secret_token = 'bdaa80f442c4eba9c269ac95a1b1304772eb337fc0c33b4719f1d2b65fb058675e88252a0aa4725ed71b93a94ec80c6f333265f4b8b8d0be7fb0d94f90b7d953'
---
+++ db/seeds.rb
@@ -0,0 +1,3 @@
+puts 'EMPTY THE MONGODB DATABASE'
+Mongoid.master.collections.reject { |c| c.name =~ /^system/}.each(&:drop)
+
