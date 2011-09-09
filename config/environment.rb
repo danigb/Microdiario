492f2a70c7667cd64c331b588007a5780f3bf32f
danigb <danigb@gmail.com>
2011-09-09 14:54:35 +0200

    Trying to let heroku precompile assets

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,10 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+# https://github.com/mongoid/mongoid/issues/648
+require 'yaml'
+YAML::ENGINE.yamler= 'syck'
+
 # Initialize the rails application
 Microdiario::Application.initialize!
+
